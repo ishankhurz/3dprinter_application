eab1dc94d1267b7cbac83de8dae99ad3635c6eca
Ishan.K <khurjekar.ishan2136@gmail.com>
2017-05-22 14:14:49 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<html>
+<frameset rows="25%,60%,*">
+  <frame src = "header.html">
+  <frameset cols="50%,*">
+    <frame src = "test.html"/>
+    <frame src = "video.html"/>
+  </frameset>
+  <frame src = "header.html">
+</frameset>  
+</html>
---
+++ myprocessingscript.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['field1'])) {
+    $path = $_POST['field1'];
+    $ret = file_put_contents('mydata.txt', $path);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
+}
+else {
+   die('no post data to process');
+}
